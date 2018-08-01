ae4d890d2c0f1cc9bd47ed5722718ca29a5e07fa
Oleksii Bukhantsov <obukhantsov@swivl.com>
2018-08-01 10:35:25 +0300

    withUnretained for SharedSequence

---
+++ Source/RxCocoa/withUnretained+RxCocoa.swift
@@ -0,0 +1,48 @@
+//
+//  withUnretained.swift
+//  RxSwiftExt
+//
+//  Created by Vincent Pradeilles on 28/03/2018.
+//  Copyright © 2018 RxSwift Community. All rights reserved.
+//
+
+import RxSwift
+import RxCocoa
+
+extension SharedSequence {
+    /**
+     Provides an unretained, safe to use (i.e. not implicitly unwrapped), reference to an object along with the events emitted by the sequence.
+     In the case the provided object cannot be retained successfully, the seqeunce will complete.
+     
+     - parameter obj: The object to provide an unretained reference on.
+     - parameter resultSelector: A function to combine the unretained referenced on `obj` and the value of the observable sequence.
+     - returns: An observable sequence that contains the result of `resultSelector` being called with an unretained reference on `obj` and the values of the original sequence.
+     */
+    public func withUnretained<T: AnyObject, Out>(_ obj: T,
+                                                  resultSelector: @escaping (T, E) -> Out?) -> SharedSequence<S, Out> {
+        return map { [weak obj] element -> Out? in
+            guard let obj = obj else {
+                return nil
+//                throw UnretainedError.failedRetaining
+                
+            }
+
+            return resultSelector(obj, element)
+        }.filter { $0 != nil }.map { $0! }
+    }
+
+    /**
+     Provides an unretained, safe to use (i.e. not implicitly unwrapped), reference to an object along with the events emitted by the sequence.
+     In the case the provided object cannot be retained successfully, the seqeunce will complete.
+     
+     - parameter obj: The object to provide an unretained reference on.
+     - returns: An observable sequence of tuples that contains both an unretained reference on `obj` and the values of the original sequence.
+     */
+    public func withUnretained<T: AnyObject>(_ obj: T) -> SharedSequence<S, (T, E)> {
+        return withUnretained(obj) { ($0, $1) }
+    }
+}
+
+private enum UnretainedError: Swift.Error {
+    case failedRetaining
+}
